9e99f0f860d71ba3372540fd1dc28c87098b285b
yeomyeom2 <yeomyeom2@gmail.com>
2023-08-08 19:51:06 +0900

    실전 강의 - 라우터

---
+++ advanced/vue-news/src/components/ToolBar.vue
@@ -0,0 +1,25 @@
+<template>
+	<nav class="nav">
+		<router-link to="/news">News</router-link>
+		<router-link to="/ask">Ask</router-link>
+		<router-link to="/jobs">Jobs</router-link>
+	</nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scope>
+.nav	{
+	padding: 20px 0;
+	background-color:rgba(0,0,0,.7);
+	color:#fff;
+	text-align: center;
+	
+}
+.nav a	{margin: 0 20px;color: inherit;font-size: 20px;text-decoration: none;}
+.nav .router-link-active	{border-bottom: 3px solid #e50c34;}
+</style>
---
+++ advanced/vue-news/src/routes/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.vue';
+import UserView from '../views/UserView';
+import ItemView from '../views/ItemView';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+	mode: 'history', //# 제거
+	routes: [
+		{
+			path: '/',
+			redirect: '/news'
+		},
+		{
+			// path: url 주소
+			path: '/news',
+			// component: url 주소로 갔을 때 표시될 컴포넌트
+			component: NewsView,
+		},
+		{
+			path: '/ask',
+			component: AskView,
+		},
+		{
+			path: '/jobs',
+			component: JobsView,
+		},
+		{
+			path: '/user',
+			component: UserView,
+		},
+		{
+			path: '/item',
+			component: ItemView,
+		}
+	]
+});
