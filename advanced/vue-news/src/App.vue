608db89c87d87aadc565633d8b5924a581ed617d
yeomyeom2 <yeomyeom2@gmail.com>
2023-09-13 19:45:51 +0900

    실전 강의 - env 환경 변수

--- advanced/vue-news/src/App.vue
+++ advanced/vue-news/src/App.vue
@@ -34,6 +34,8 @@ export default {
 	created() {
 		bus.$on('start:spinner',  this.startSpinner);
 		bus.$on('end:spinner', this.endSpinner);
+
+		console.log(process.env.VUE_APP_TITLE);
 	},
 	beforeDestroy() {
 		bus.$off('start:spinner', this.startSpinner);
