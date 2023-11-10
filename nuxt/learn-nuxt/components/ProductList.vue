6b4ace58a06d0792da4dd165da991b4c0cd4cbc6
yeomyeom2 <yeomyeom2@gmail.com>
2023-11-10 19:52:56 +0900

    Nuxt.js - Nuxt의 데이터 호출 방식과 API 연동

---
+++ nuxt/learn-nuxt/components/ProductList.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+	{{ product }}
+  </div>
+</template>
+
+<script setup>
+const product = await $fetch('http://localhost:3000/products').catch((error) => error.data);
+</script>
+
+<style>
+
+</style>
--- nuxt/learn-nuxt/pages/main.vue
+++ nuxt/learn-nuxt/pages/main.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
 	<p>메인 페이지입니다</p>
+	<!-- <div>
+		{{ response }}
+	</div> -->
+	<ProductList></ProductList>
   </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import ProductList from '../components/ProductList.vue'
 
-}
+// const response = await $fetch('http://localhost:3000/products').catch((error) => error.data);
 </script>
 
 <style>
