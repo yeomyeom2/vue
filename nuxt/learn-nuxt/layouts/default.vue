0e2bba07eefaee3b7f0d78d301a8adc9a72a8308
yeomyeom2 <yeomyeom2@gmail.com>
2023-11-10 17:15:31 +0900

    Nuxt.js - 프로젝트 시작하기

---
+++ nuxt/learn-nuxt/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<header>
+			<h1>{{ $route.name }} 페이지</h1>
+			<!-- <nuxt-link to="/">홈페이지</nuxt-link>
+			<nuxt-link to="/main">메인 페이지</nuxt-link>
+			<nuxt-link to="/product">상품 페이지</nuxt-link> -->
+			<NuxtLink to="/">홈페이지</NuxtLink>
+			<NuxtLink to="/main">메인 페이지</NuxtLink>
+			<NuxtLink to="/product">상품 페이지</NuxtLink>
+		</header>
+	  <slot />
+	</div>
+  </template>
+  
+  <script>
+  export default {
+	created() {
+		console.log(this.$route)
+	}
+  }
+  </script>
+  
+  <style>
+  
+  </style>
---
+++ nuxt/learn-nuxt/pages/product.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+	<ul class="list">상품 1</ul>
+	<ul class="list">상품 2</ul>
+	<ul class="list">상품 3</ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
